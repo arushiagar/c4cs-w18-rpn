74da2272ddadcad038239e560bef1c1e378b851c
Arushi Agarwal <arushia@umich.edu>
2018-03-05 11:00:08 -0500

    color

--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,6 @@ class TestBasics(unittest.TestCase):
     def test_carat(self):
         result = rpn.calculate("2 3 ^")
         self.assertEqual(8, result)
+    def test_error(self):
+        with self.assertRaises(ValueError):
+            rpn.calculate("3 3 + ")
